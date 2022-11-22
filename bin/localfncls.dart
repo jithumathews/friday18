c26f91ed49cac2eb393a31bfac7d6ad5ea60672a
JITHU MATHEWS <jithumathews111@gmail.com>
2022-11-22 15:10:39 +0530

    commit

---
+++ bin/localfncls.dart
@@ -0,0 +1,43 @@
+class Mathss{
+  void add()
+  {
+    int a = 10,
+        b = 20,
+        sum = a+b;
+    print('sum = $sum');
+  }
+
+  void sub()
+  {
+    int a = 30,
+        b = 20,
+        sub = a-b;
+    print('sub = $sub');
+  }
+
+  void mul()
+  {
+    int a = 10,
+        b = 2,
+        mul = a*b;
+    print('mul = $mul');
+  }
+
+  void div()
+  {
+    int a = 10,
+        b = 3,
+        div = a~/b;
+    print('div = $div');
+  }
+
+}
+
+
+void main(){
+  Mathss obj = Mathss();
+  obj.add();
+  obj.sub();
+  obj.mul();
+  obj.div();
+}
