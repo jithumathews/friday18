5224421e732e68d53c54a28dfec8d8c5a9b717da
JITHU MATHEWS <jithumathews111@gmail.com>
2022-11-23 14:47:38 +0530

    commit

---
+++ bin/finalkey.dart
@@ -0,0 +1,26 @@
+void main(){
+  String name = "anna";
+  int age = 20;
+  final String course = 'Flutter';
+  const int pincode = 685571;
+
+  print(name);
+  print(age);
+  print(course);
+  print(pincode);
+
+   name = "hari";
+  age = 22;
+  //course = 'python';
+  //pincode = 565656;
+
+
+  print(name);
+  print(age);
+  print(course);
+  print(pincode);
+
+
+
+
+}
